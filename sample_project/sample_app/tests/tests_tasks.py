f639722ef74a388dad5a25874e7bf8b81a7ee027
lucasgomide <lucaslg200@gmail.com>
2022-02-17 09:21:58 -0300

    feat: add Routine's Task
    In order to execute a routine async we have to wrap a task then handle any error.
    This commits also adds a task interface supporting revert a job execution

--- sample_project/sample_app/tests/tests_tasks.py
+++ sample_project/sample_app/tests/tests_tasks.py
@@ -2,14 +2,12 @@ import json
 from unittest.mock import patch
 
 from django.apps import apps
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from gcp_pilot.exceptions import DeletedRecently
 from gcp_pilot.mocker import patch_auth
-
-from django_cloud_tasks import exceptions
-from django_cloud_tasks.tasks import PublisherTask
+from django_cloud_tasks import exceptions, factories
+from django_cloud_tasks.tasks import PublisherTask, PipelineRoutineTask
 from sample_project.sample_app import tasks
-
 
 class TasksTest(SimpleTestCase):
     def patch_push(self, **kwargs):
@@ -18,7 +16,7 @@ class TasksTest(SimpleTestCase):
     def test_registered_tasks(self):
         app_config = apps.get_app_config("django_cloud_tasks")
 
-        expected_tasks = {"PublisherTask", "CalculatePriceTask", "FailMiserablyTask", "OneBigDedicatedTask"}
+        expected_tasks = {"PublisherTask", "CalculatePriceTask", "FailMiserablyTask", "OneBigDedicatedTask", "PipelineRoutineTask", "SayHelloTask", "DummyRoutineTask"}
         self.assertEqual(expected_tasks, set(app_config.on_demand_tasks))
 
         expected_tasks = {"SaySomethingTask"}
@@ -87,3 +85,74 @@ class TasksTest(SimpleTestCase):
             with patch_auth():
                 response = tasks.CalculatePriceTask().delay(price=30, quantity=4, discount=0.2)
         self.assertGreater(response, 0)
+
+
+class PipelineRoutineTaskTest(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        patched_settings = self.settings(EAGER_TASKS=True)
+        patched_settings.enable()
+        self.addCleanup(patched_settings.disable)
+
+    def tests_dont_process_completed_routine(self):
+        routine = factories.RoutineWithoutSignalFactory(
+            status="completed",
+            task_name="SayHelloTask",
+        )
+        with self.assertLogs(level='INFO') as cm:
+            PipelineRoutineTask().delay(routine_id=routine.pk)
+            self.assertEqual(cm.output, [f"INFO:root:Routine #{routine.pk} is already completed"])
+
+    def tests_start_pipeline_revert_flow_if_exceeded_retries(self):
+        routine = factories.RoutineWithoutSignalFactory(
+            status="running",
+            task_name="SayHelloTask",
+            max_retries=1,
+            attempt_count=2,
+        )
+        with patch("django_cloud_tasks.models.Pipeline.revert") as revert:
+            with self.assertLogs(level='INFO') as context:
+                PipelineRoutineTask().delay(routine_id=routine.pk)
+                self.assertEqual(context.output, [
+                    f"INFO:root:Routine #{routine.id} has exhausted retries and is being reverted",
+                ])
+                revert.assert_called_once()
+
+    def tests_store_task_output_into_routine(self):
+        routine = factories.RoutineWithoutSignalFactory(
+            status="running",
+            task_name="SayHelloTask",
+            body={"attributes": [1,2,3]},
+            attempt_count=1,
+        )
+        with self.assertLogs(level='INFO') as cm:
+            PipelineRoutineTask().run(routine_id=routine.pk)
+            routine.refresh_from_db()
+            self.assertEqual(cm.output, [
+                f"INFO:root:Routine #{routine.id} is running",
+                f"INFO:root:Routine #{routine.id} just completed",
+            ])
+            self.assertEqual("completed", routine.status)
+            self.assertEqual(2, routine.attempt_count)
+
+    def tests_fail_routine_if_task_has_failed(self):
+        routine = factories.RoutineWithoutSignalFactory(
+            status="running",
+            task_name="SayHelloTask",
+            body={"attributes": [1,2,3]},
+            attempt_count=1,
+        )
+        with self.assertLogs(level='INFO') as cm:
+            with patch("sample_project.sample_app.tasks.SayHelloTask.run", side_effect=Exception("any error")):
+                with patch("django_cloud_tasks.models.Routine.enqueue") as enqueue:
+                    PipelineRoutineTask().run(routine_id=routine.pk)
+                    routine.refresh_from_db()
+                    self.assertEqual(cm.output, [
+                        f"INFO:root:Routine #{routine.id} is running",
+                        f"INFO:root:Routine #{routine.id} has failed",
+                        f"INFO:root:Routine #{routine.id} has been enqueued for retry",
+                    ])
+                    self.assertEqual("failed", routine.status)
+                    enqueue.assert_called_once()
+                    self.assertEqual(2, routine.attempt_count)
