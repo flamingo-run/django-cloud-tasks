97573c02b27682992bae80f4585e3c370fcb83d7
lucasgomide <lucaslg200@gmail.com>
2022-02-17 06:44:22 -0300

    feat: enqueue next routines when parent is completed

--- django_cloud_tasks/signals.py
+++ django_cloud_tasks/signals.py
@@ -11,3 +11,17 @@ def ensure_valid_task_name(sender, instance, **kwargs):
         instance.task
     except exceptions.TaskNotFound:
         raise ValidationError(f"The task {instance.task_name} was not found. Make sure {instance.task_name} is properly set.")
+
+@receiver(pre_save, sender=models.Routine)
+def enqueue_next_routines(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    current_routine = models.Routine.objects.get(pk=instance.pk)
+
+    if current_routine.status == instance.status:
+        return
+
+    if instance.status == models.Routine.Statuses.COMPLETED:
+        for routine in instance.next_routines.all():
+            routine.enqueue()
