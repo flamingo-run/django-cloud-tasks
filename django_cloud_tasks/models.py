96c56813ba46acfe8404140bdfa037e46e5b49e1
lucasgomide <lucaslg200@gmail.com>
2022-02-17 06:23:24 -0300

    feat: add Pipeline's models
    The following models support handle complex Task pipeline flow

---
+++ django_cloud_tasks/models.py
@@ -0,0 +1,131 @@
+from typing import Optional, Dict
+from django.db import transaction
+from django.apps import apps
+
+from datetime import datetime
+from django.db import models
+from django_cloud_tasks import tasks, serializers
+
+
+class Pipeline(models.Model):
+    name = models.CharField(max_length=100)
+
+    def start(self):
+        routines = self.routines.filter(dependent_routines__id__isnull=True)
+        for routine in routines:
+            routine.enqueue()
+
+    def revert(self):
+        routines = self.routines.filter(next_routines__id__isnull=True)
+        for routine in routines:
+            routine.revert()
+
+
+class Routine(models.Model):
+    class Statuses(models.TextChoices):
+        PENDING = ("pending", "Pending")
+        SCHEDULED = ("scheduled", "Scheduled")
+        RUNNING = ("running", "Running")
+        COMPLETED = ("completed", "Completed")
+        FAILED = ("failed", "Failed")
+        REVERTING = ("reverting", "Reverting")
+        REVERTED = ("reverted", "Reverted")
+        ABORTED = ("aborted", "Aborted")
+
+    task_name = models.CharField(max_length=100)
+    pipeline = models.ForeignKey(
+        to="django_cloud_tasks.Pipeline",
+        related_name="routines",
+        on_delete=models.PROTECT,
+    )
+    body = models.JSONField(
+        default=dict,
+        encoder=serializers.JSONEncoder,
+    )
+    attempt_count = models.PositiveIntegerField(default=0)
+    max_retries = models.PositiveIntegerField(null=True)
+    output = models.JSONField(
+        null=True,
+        blank=True,
+        encoder=serializers.JSONEncoder,
+    )
+    starts_at = models.DateTimeField(null=True, blank=True)
+    ends_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=Statuses.choices,
+        default=Statuses.PENDING,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    next_routines = models.ManyToManyField(
+        to="Routine",
+        through="RoutineVertex",
+        through_fields=("routine", "next_routine"),
+        related_name="dependent_routines",
+    )
+
+    def fail(self, output: Dict):
+        self.output = output
+        self.status = self.Statuses.FAILED
+        self.ends_at = datetime.now()
+        self.save()
+
+    def complete(self, output: Dict):
+        self.output = output
+        self.status = self.Statuses.COMPLETED
+        self.ends_at = datetime.now()
+        self.save()
+
+    def enqueue(self):
+        with transaction.atomic():
+            self.status = self.Statuses.SCHEDULED
+            self.starts_at = datetime.now()
+            self.save()
+            tasks.RoutineTask().delay(routine_id=self.pk)
+
+    def revert(self):
+        with transaction.atomic():
+            if self.status == self.Statuses.COMPLETED:
+                self.status = self.Statuses.REVERTING
+                self.save()
+                meta = {"routine_id": self.pk}
+                self.task().revert(data=self.output, _meta=meta)
+                return
+
+            self.status = self.Statuses.ABORTED
+            self.save()
+
+    def task(self) -> Optional[tasks.Task]:
+        app = apps.get_app_config("django_cloud_tasks")
+        tasks = app.on_demand_tasks.copy()
+        return tasks.get(self.task_name)
+
+
+class RoutineVertex(models.Model):
+    next_routine = models.ForeignKey(
+        to="django_cloud_tasks.Routine",
+        on_delete=models.PROTECT,
+        related_name="required_routine_vertices",
+    )
+    routine = models.ForeignKey(
+        to="django_cloud_tasks.Routine",
+        related_name="next_routine_vertices",
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(name="unique_routine_next_routine", fields=("next_routine", "routine")),
+        ]
+
+
+__all__ = (
+    "Routine",
+    "RoutineVertex",
+    "Pipeline",
+)
---
+++ sample_project/sample_app/tests/tests_models.py
@@ -0,0 +1,99 @@
+from unittest.mock import patch, call
+from datetime import datetime
+from freezegun import freeze_time
+from django.test import TestCase
+from django_cloud_tasks import factories
+
+
+class RoutineModelTest(TestCase):
+    @freeze_time("2020-01-01")
+    def tests_fail(self):
+        routine = factories.RoutineFactory(output=None, ends_at=None)
+        error = {"error": "something went wrong"}
+        routine.fail(output=error)
+        routine.refresh_from_db()
+        self.assertEqual("failed", routine.status)
+        self.assertEqual(error, routine.output)
+        self.assertEqual(datetime.now(), routine.ends_at.replace(tzinfo=None))
+
+    @freeze_time("2020-01-01")
+    def tests_complete(self):
+        routine = factories.RoutineFactory(output=None, ends_at=None)
+        output = {"id": 42}
+        routine.complete(output=output)
+        routine.refresh_from_db()
+        self.assertEqual("completed", routine.status)
+        self.assertEqual(output, routine.output)
+        self.assertEqual(datetime.now(), routine.ends_at.replace(tzinfo=None))
+
+    @freeze_time("2020-01-01")
+    def tests_enqueue(self):
+        routine = factories.RoutineFactory()
+        with patch("django_cloud_tasks.tasks.RoutineTask.delay") as task:
+            routine.enqueue()
+            routine.refresh_from_db()
+            self.assertEqual("scheduled", routine.status)
+            self.assertEqual(datetime.now(), routine.starts_at.replace(tzinfo=None))
+        task.assert_called_once_with(routine_id=routine.pk)
+
+    def tests_revert_completed_routine(self):
+        routine = factories.RoutineFactory(status="completed", output="{'id': 42}")
+        with patch("django_cloud_tasks.factories.DummyRoutineTask.revert") as revert_task:
+            routine.revert()
+            routine.refresh_from_db()
+            self.assertEqual("reverting", routine.status)
+        revert_task.assert_called_once_with(data=routine.output, _meta={"routine_id": routine.pk})
+
+    def tests_revert_not_processed_routine(self):
+        routine = factories.RoutineFactory()
+        with patch("django_cloud_tasks.factories.DummyRoutineTask.revert") as revert_task:
+            routine.revert()
+            routine.refresh_from_db()
+            self.assertEqual("aborted", routine.status)
+        revert_task.assert_not_called()
+
+class PipelineModelTest(TestCase):
+    def tests_start_pipeline(self):
+        pipeline = factories.PipelineFactory()
+        second_routine = factories.RoutineFactory()
+        pipeline.routines.add(second_routine)
+        third_routine = factories.RoutineFactory()
+        pipeline.routines.add(third_routine)
+        first_routine = factories.RoutineFactory()
+        pipeline.routines.add(first_routine)
+
+        another_first_routine = factories.RoutineFactory()
+        pipeline.routines.add(another_first_routine)
+
+        factories.RoutineVertexFactory(routine=second_routine, next_routine=third_routine)
+        factories.RoutineVertexFactory(routine=first_routine, next_routine=second_routine)
+
+        with patch("django_cloud_tasks.tasks.RoutineTask.delay") as task:
+            pipeline.start()
+        calls = [call(routine_id=first_routine.pk), call(routine_id=another_first_routine.pk)]
+        task.assert_has_calls(calls, any_order=True)
+
+    def tests_revert_pipeline(self):
+        pipeline = factories.PipelineFactory()
+        second_routine = factories.RoutineFactory()
+        pipeline.routines.add(second_routine)
+
+        third_routine = factories.RoutineWithoutSignalFactory(status="completed")
+        pipeline.routines.add(third_routine)
+
+        first_routine = factories.RoutineFactory()
+        pipeline.routines.add(first_routine)
+
+        fourth_routine = factories.RoutineWithoutSignalFactory(status="completed")
+        pipeline.routines.add(fourth_routine)
+
+        factories.RoutineVertexFactory(routine=second_routine, next_routine=third_routine)
+        factories.RoutineVertexFactory(routine=first_routine, next_routine=second_routine)
+
+        with patch("django_cloud_tasks.tasks.RoutineTask.revert") as task:
+            pipeline.revert()
+        calls = [
+            call(data=fourth_routine.output, _meta={"routine_id": fourth_routine.pk}),
+            call(data=third_routine.output, _meta={"routine_id": third_routine.pk})
+        ]
+        task.assert_has_calls(calls, any_order=True)
