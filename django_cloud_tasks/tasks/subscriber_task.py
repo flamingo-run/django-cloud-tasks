4cd7afc90562aecacd78ea4230a27eb02064dc0b
Joao Daher <joao@daher.dev>
2022-09-26 12:37:23 -0300

    feat: add support to Subscriber dead-letter, retry and expiration
    +1 dead

--- django_cloud_tasks/tasks/subscriber_task.py
+++ django_cloud_tasks/tasks/subscriber_task.py
@@ -10,7 +10,11 @@ class SubscriberTask(Task):
     abstract = True
     _use_oidc_auth = True
     _url_name = "subscriptions-endpoint"
-    enable_message_ordering = False
+    enable_message_ordering: bool = False
+    max_retries: int | None = None
+    min_backoff: int | None = None
+    max_backoff: int | None = None
+    expiration_ttl: int | None = None
 
     def _body_to_kwargs(self, request_body):
         message = Message.load(body=request_body)
@@ -30,12 +34,26 @@ class SubscriberTask(Task):
             enable_message_ordering=self.enable_message_ordering,
             push_to_url=self.url(),
             use_oidc_auth=self._use_oidc_auth,
+            dead_letter_topic_id=self.dead_letter_topic_name,
+            dead_letter_subscription_id=self.dead_letter_subscription_name,
+            max_retries=self.max_retries,
+            min_backoff=self.min_backoff,
+            max_backoff=self.max_backoff,
+            expiration_ttl=self.expiration_ttl,
         )
 
     @property
     @abstractmethod
     def topic_name(self):
         raise NotImplementedError()
+
+    @property
+    def dead_letter_topic_name(self):
+        return None
+
+    @property
+    def dead_letter_subscription_name(self):
+        return self.dead_letter_topic_name
 
     @property
     def subscription_name(self):
