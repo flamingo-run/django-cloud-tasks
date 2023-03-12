5be5ebe28978408071a642ac42b3bdbd96f8fd30
lucasgomide <lucaslg200@gmail.com>
2023-03-12 10:22:41 -0300

    feat: add drf_kit library
    This library has a very useful models helper, such as ModelDiffMixin which says which fields & their values have been changed. So you can decrease hits on database

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -102,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+# RestFul
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%SZ",
+}
