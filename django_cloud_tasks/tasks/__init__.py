cb6ffb99fdf1284ac2da99595d0cd332efd39899
Joao Daher <joao@daher.dev>
2023-03-15 17:13:40 -0300

    chore: update tasks exposed to import

--- django_cloud_tasks/tasks/__init__.py
+++ django_cloud_tasks/tasks/__init__.py
@@ -1,15 +1,23 @@
 from django_cloud_tasks.tasks.periodic_task import PeriodicTask
-from django_cloud_tasks.tasks.publisher_task import PublisherTask
-from django_cloud_tasks.tasks.routine_task import PipelineRoutineRevertTask, PipelineRoutineTask, RoutineTask
+from django_cloud_tasks.tasks.publisher_task import PublisherTask, ModelPublisherTask
+from django_cloud_tasks.tasks.routine_task import (
+    PipelineDispatcherTask,
+    RoutineReverterTask,
+    RoutineExecutorTask,
+    RoutineTask,
+)
 from django_cloud_tasks.tasks.subscriber_task import SubscriberTask
-from django_cloud_tasks.tasks.task import Task
+from django_cloud_tasks.tasks.task import Task, TaskMetadata
 
 __all__ = (
     "Task",
+    "TaskMetadata",
     "PeriodicTask",
+    "ModelPublisherTask",
     "PublisherTask",
     "SubscriberTask",
     "RoutineTask",
-    "PipelineRoutineTask",
-    "PipelineRoutineRevertTask",
+    "PipelineDispatcherTask",
+    "RoutineExecutorTask",
+    "RoutineReverterTask",
 )
