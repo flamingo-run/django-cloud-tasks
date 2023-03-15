61a740e80e87ed3f0942473fbf24324d09973354
Joao Daher <joao@daher.dev>
2023-03-15 17:11:10 -0300

    refactor: Routine Task
    It now has classmethods
    Some tasks have been renamed to make it clear which processes and which reverts
    Some database saves have an extra safety measure to update only the desired fields

--- django_cloud_tasks/tasks/routine_task.py
+++ django_cloud_tasks/tasks/routine_task.py
@@ -1,6 +1,5 @@
-# pylint: disable=no-member
+import abc
 import logging
-from abc import abstractmethod
 
 from django.core.cache import cache
 
@@ -10,19 +9,14 @@ from django_cloud_tasks.tasks.task import Task
 logger = logging.getLogger()
 
 
-class RoutineTask(Task):
-    abstract = True
-
-    @abstractmethod
-    def run(self, **kwargs):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def revert(self, data: dict):
+class RoutineTask(Task, abc.ABC):
+    @classmethod
+    @abc.abstractmethod
+    def revert(cls, data: dict):
         raise NotImplementedError()
 
 
-class RoutineLockTaskMixin(Task):
+class PipelineDispatcherTask(Task, abc.ABC):
     WAIT_FOR_LOCK = 5  # in seconds
     LOCK_EXPIRATION = 60  # in seconds
 
@@ -30,30 +24,29 @@ class RoutineLockTaskMixin(Task):
         lock_key = f"lock-{self.__class__.__name__}-{routine_id}"
         routine_lock = cache.lock(key=lock_key, timeout=self.LOCK_EXPIRATION, blocking_timeout=self.WAIT_FOR_LOCK)
         with routine_lock:
-            self._run(routine_id=routine_id)
+            routine = models.Routine.objects.get(pk=routine_id)
+            return self.process_routine(routine=routine)
 
-    @abstractmethod
-    def _run(self, routine_id: int):
+    @abc.abstractmethod
+    def process_routine(self, routine: models.Routine):
         raise NotImplementedError()
 
 
-class PipelineRoutineRevertTask(RoutineLockTaskMixin):
-    def _run(self, routine_id: int):
-        routine = models.Routine.objects.get(pk=routine_id)
-        routine.task().revert(data=routine.output)
+class RoutineReverterTask(PipelineDispatcherTask):
+    def process_routine(self, routine: models.Routine):
+        routine.task_class.revert(data=routine.output)
         routine.status = models.Routine.Statuses.REVERTED
-        routine.save()
+        routine.save(update_fields=("status",))
 
 
-class PipelineRoutineTask(RoutineLockTaskMixin):
-    def _run(self, routine_id: int):
-        routine = models.Routine.objects.get(pk=routine_id)
+class RoutineExecutorTask(PipelineDispatcherTask):
+    def process_routine(self, routine: models.Routine):
         if routine.status == models.Routine.Statuses.COMPLETED:
-            logger.info(f"Routine #{routine_id} is already completed")
+            logger.info(f"Routine #{routine.pk} is already completed")
             return
 
         if routine.max_retries and routine.attempt_count >= routine.max_retries:
-            error_message = f"Routine #{routine_id} has exhausted retries and is being reverted"
+            error_message = f"Routine #{routine.pk} has exhausted retries and is being reverted"
             logger.info(error_message)
             routine.fail(output={"error": error_message})
             routine.pipeline.revert()
@@ -61,17 +54,17 @@ class PipelineRoutineTask(RoutineLockTaskMixin):
 
         routine.attempt_count += 1
         routine.status = models.Routine.Statuses.RUNNING
-        routine.save()
+        routine.save(update_fields=("attempt_count", "status", "updated_at"))
 
         try:
-            logger.info(f"Routine #{routine_id} is running")
-            task_response = routine.task().run(**routine.body)
-        except Exception as error:  # pylint: disable=no-member,broad-except
-            logger.info(f"Routine #{routine_id} has failed")
+            logger.info(f"Routine #{routine.pk} is running")
+            task_response = routine.task_class(metadata=self._metadata).sync(**routine.body)
+        except Exception as error:
+            logger.info(f"Routine #{routine.pk} has failed")
             routine.fail(output={"error": str(error)})
             routine.enqueue()
-            logger.info(f"Routine #{routine_id} has been enqueued for retry")
+            logger.info(f"Routine #{routine.pk} has been enqueued for retry")
             return
 
         routine.complete(output=task_response)
-        logger.info(f"Routine #{routine_id} just completed")
+        logger.info(f"Routine #{routine.pk} just completed")
