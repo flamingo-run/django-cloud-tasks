2ad37bc5fc1a951149f87a714455c8e2527d9eb2
Joao Daher <joao@daher.dev>
2023-03-22 14:42:30 -0300

    test: add tests to PubSub headers propagation

--- sample_project/sample_app/tests/tests_views.py
+++ sample_project/sample_app/tests/tests_views.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch, ANY
 
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TransactionTestCase
+from gcp_pilot.pubsub import Message
 
 
 class TaskViewTest(SimpleTestCase):
@@ -103,3 +104,57 @@ class TaskViewTest(SimpleTestCase):
 
         expected_content = {"Traceparent": "trace-this-potato"}
         self.assertEqual(expected_content, response.json()["result"])
+
+
+class SubscriberTaskViewTest(SimpleTestCase):
+    def url(self, name):
+        return f"/subscriptions/{name}"
+
+    def trigger_subscriber(self, content, attributes):
+        url = "/subscriptions/ParentSubscriberTask"
+        message = Message(
+            id="i-dont-care",
+            data=content,
+            attributes=attributes,
+            subscription="potato",
+        )
+        return self.client.post(path=url, data=message.dump(), content_type="application/json")
+
+    def test_propagate_headers(self):
+        content = {
+            "price": 10,
+            "quantity": 42,
+        }
+        attributes = {
+            "HTTP_traceparent": "trace-this-potato",
+            "HTTP_another-random-header": "please-do-not-propagate-this",
+        }
+
+        with patch("gcp_pilot.tasks.CloudTasks.push") as push:
+            with patch("django_cloud_tasks.tasks.TaskMetadata.from_task_obj"):
+                self.trigger_subscriber(content=content, attributes=attributes)
+
+        expected_kwargs = {
+            "queue_name": "tasks",
+            "url": "http://localhost:8080/tasks/CalculatePriceTask",
+            "payload": '{"price": 10, "quantity": 42}',
+            "headers": {"Traceparent": "trace-this-potato", "X-CloudTasks-Projectname": ANY},
+        }
+        push.assert_called_once_with(**expected_kwargs)
+
+
+class PublisherTaskTest(TransactionTestCase):
+    def test_propagate_headers(self):
+        url = "/create-person"
+        data = {"name": "Harry Potter"}
+        headers = {
+            "traceparent": "trace-this-potato",
+            "another-random-header": "please-do-not-propagate-this",
+        }
+        django_headers = {f"HTTP_{key.upper()}": value for key, value in headers.items()}
+
+        with patch("gcp_pilot.pubsub.CloudPublisher.publish") as publish:
+            self.client.post(path=url, data=data, content_type="application/json", **django_headers)
+
+        expected_attributes = {"HTTP_Traceparent": "trace-this-potato", "any-custom-attribute": "yay!"}
+        publish.assert_called_once_with(message=ANY, topic_id=ANY, attributes=expected_attributes)
