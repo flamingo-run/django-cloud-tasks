ac4047ed4832936ba3346821f7106db1e61dc4ba
Rodrigo Almeida <ralmeida@nilo.co>
2023-11-03 22:31:32 -0300

    feat: add X-Cloudscheduler header support to TaskMetadata

--- sample_project/sample_app/tests/tests_tasks/tests_tasks.py
+++ sample_project/sample_app/tests/tests_tasks/tests_tasks.py
@@ -279,6 +279,21 @@ class TestTaskMetadata(TestCase):
             execution_number=7,
             dispatch_number=1,
             eta=self.some_date,
+            is_cloud_scheduler=False,
+        )
+
+    @property
+    def sample_cloud_scheduler_metadata(self) -> TaskMetadata:
+        return TaskMetadata(
+            project_id="wizard-project",
+            queue_name="wizard-queue",
+            task_id="hp-1234567",
+            execution_number=7,
+            dispatch_number=1,
+            eta=self.some_date,
+            is_cloud_scheduler=True,
+            cloud_scheduler_job_name="wizard-api--LevitationTask",
+            cloud_scheduler_schedule_time=datetime(2023, 11, 3, 15, 27, 0, tzinfo=UTC),
         )
 
     def test_create_from_headers(self):
@@ -291,6 +306,23 @@ class TestTaskMetadata(TestCase):
         self.assertEqual("wizard-project", metadata.project_id)
         self.assertEqual("wizard-queue", metadata.queue_name)
         self.assertEqual("hp-1234567", metadata.task_id)
+        self.assertFalse(metadata.is_cloud_scheduler)
+        self.assertIsNone(metadata.cloud_scheduler_job_name)
+        self.assertIsNone(metadata.cloud_scheduler_schedule_time)
+
+    def test_create_from_cloud_schedule_headers(self):
+        metadata = TaskMetadata.from_headers(
+            headers=self.sample_headers
+            | {
+                "X-Cloudscheduler": "true",
+                "X-Cloudscheduler-Scheduletime": "2023-11-03T08:27:00-07:00",
+                "X-Cloudscheduler-Jobname": "wizard-api--LevitationTask",
+            }
+        )
+
+        self.assertTrue(metadata.is_cloud_scheduler)
+        self.assertEqual("wizard-api--LevitationTask", metadata.cloud_scheduler_job_name)
+        self.assertEqual(datetime(2023, 11, 3, 15, 27, 0, tzinfo=UTC), metadata.cloud_scheduler_schedule_time)
 
     def test_build_headers(self):
         headers = self.sample_metadata.to_headers()
@@ -301,6 +333,22 @@ class TestTaskMetadata(TestCase):
         self.assertEqual("wizard-project", headers["X-Cloudtasks-Projectname"])
         self.assertEqual("wizard-queue", headers["X-Cloudtasks-Queuename"])
         self.assertEqual("hp-1234567", headers["X-Cloudtasks-Taskname"])
+        self.assertNotIn("X-Cloudscheduler", headers)
+        self.assertNotIn("X-Cloudscheduler-Scheduletime", headers)
+        self.assertNotIn("X-Cloudscheduler-Jobname", headers)
+
+    def test_build_cloud_scheduler_headers(self):
+        headers = self.sample_cloud_scheduler_metadata.to_headers()
+
+        self.assertEqual("7", headers["X-Cloudtasks-Taskexecutioncount"])
+        self.assertEqual("1", headers["X-Cloudtasks-Taskretrycount"])
+        self.assertEqual(str(int(self.some_date.timestamp())), headers["X-Cloudtasks-Tasketa"])
+        self.assertEqual("wizard-project", headers["X-Cloudtasks-Projectname"])
+        self.assertEqual("wizard-queue", headers["X-Cloudtasks-Queuename"])
+        self.assertEqual("hp-1234567", headers["X-Cloudtasks-Taskname"])
+        self.assertEqual("true", headers["X-Cloudscheduler"])
+        self.assertEqual("2023-11-03T15:27:00+00:00", headers["X-Cloudscheduler-Scheduletime"])
+        self.assertEqual("wizard-api--LevitationTask", headers["X-Cloudscheduler-Jobname"])
 
     def test_comparable(self):
         reference = self.sample_metadata
